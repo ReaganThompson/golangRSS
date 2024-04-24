72476e940158af547a2546c356b427206683993f
Reagan Thompson <reagan.r.thompson@gmail.com>
2024-04-24 09:31:58 -0700

    Added creating and getting feeds

--- main.go
+++ main.go
@@ -53,11 +53,16 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	// Router Connections
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
 	router.Mount("/v1", v1Router)
 
--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -5,7 +5,7 @@ CREATE TABLE feeds (
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
     url TEXT UNIQUE NOT NULL,
-    user_id UUID references users(id) ON DELETE CASCADE
+    user_id UUID NOT NULL references users(id) ON DELETE CASCADE
 );
 
 
